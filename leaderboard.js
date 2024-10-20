3a9615d4382fccff38515a9b098e522fe2a83a23
Ricky <rickybeechingdev@gmail.com>
2024-10-20 18:14:21 +0100

    initial commit

---
+++ leaderboard.js
@@ -0,0 +1,47 @@
+const leaderboard_container = document.querySelector(".leaderboard");
+const backendURL = "https://jumbl-backend.onrender.com";
+
+const leaderboard_item_template = (rank, username, score) =>`<div class="leaderboard__item item">
+            <p class="leaderboard__place">${rank}</p>
+            <p class="leaderboard__user">${username}</p>
+            <p class="leaderboard__score">${score}pts</p>
+        </div>`
+
+//populateLeaderboard();
+
+window.onload = (e) => {
+    populateLeaderboard();
+}
+
+function refresh() {
+    window.location.reload();
+}
+
+function goToGame() {
+    window.location.assign("./index.html")
+}
+
+async function populateLeaderboard(){
+    const usersByScore = await getLeaderboard();
+    usersByScore.forEach(user => {
+        leaderboard_container.innerHTML += leaderboard_item_template(usersByScore.indexOf(user) + 1, user.username, user.highscore)
+    });
+}
+
+async function getLeaderboard(){
+    try
+    {
+        const response = await fetch(backendURL + "/leaderboard", {
+            method: "GET",
+            headers: {"Content-Type" : "application/json"}
+        })
+
+        const data = await response.json();
+        return data.users;
+    } 
+    catch (err)
+    {
+        console.error(err)
+    }
+}
+
